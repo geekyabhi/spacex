9b5cde96e09c892fd8ea99e68e0a47783022a7a5
Abhinav Singh <thakurabhinav17122001@gmail.com>
2021-04-05 19:43:43 +0530

    first commit

---
+++ schema.js
@@ -0,0 +1,63 @@
+const { default: axios } = require('axios')
+const {GraphQLObjectType,GraphQLInt,GraphQLString, GraphQLBoolean, GraphQLList, GraphQLSchema} =require('graphql')
+
+const LaunchType=new GraphQLObjectType({
+    name:'Launch',
+    fields:()=>({
+        flight_number:{type:GraphQLInt},
+        mission_name:{type:GraphQLString},
+        launch_year:{type:GraphQLString},
+        launch_date_local:{type:GraphQLString},
+        launch_success:{type:GraphQLBoolean},
+        rocket:{type:RocketType},
+    })
+})
+
+const RocketType =new GraphQLObjectType({
+    name:'Rocket',
+    fields:()=>({
+        rocket_id:{type:GraphQLString},
+        rocket_name:{type:GraphQLString},
+        rocket_type:{type:GraphQLString}
+    })
+})
+
+const RootQuery=new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:{
+        launches:{
+            type:new GraphQLList(LaunchType),
+            resolve(parent,args){
+                return axios.get('https://api.spacexdata.com/v3/launches').then(res=>res.data)
+            }
+        },
+        launch:{
+            type:LaunchType,
+            args:{
+                flight_number:{type:GraphQLInt}
+            },
+            resolve(parent,{flight_number}){
+                return axios.get(`https://api.spacexdata.com/v3/launches/${flight_number}`).then(res=>res.data)
+            }
+        },
+        rockets:{
+            type:new GraphQLList(RocketType),
+            resolve(parent,args){
+                return axios.get('https://api.spacexdata.com/v3/rockets').then(res=>res.data)
+            }
+        },
+        rocket:{
+            type:RocketType,
+            args:{
+                id:{type:GraphQLString}
+            },
+            resolve(parent,{id}){
+                return axios.get(`https://api.spacexdata.com/v3/rockets/${id}`).then(res=>res.data)
+            }
+        }
+    }
+})
+
+module.exports=new GraphQLSchema({
+    query:RootQuery
+})
